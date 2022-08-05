891b5792851a406fed900a8be65c6982ef506d6f
Arjun Pandit <arjunpandit0840@gmail.com>
2022-08-06 03:37:06 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (89.05%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -2,17 +2,12 @@ class Solution {
     public int[] shuffle(int[] nums, int n) {
         if(nums.length<4) return nums;
         boolean flag= true;
-        int j=0;
         int[] res = new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-            if(flag==true){
+        for(int i=0,j=0;i<nums.length;i++){
+            if(flag)
                 res[i]=nums[j];
-                
-            }
-            if(flag==false){
-                res[i]=nums[(nums.length/2)+j++];
-               
-            }
+            if(!flag)
+                res[i]=nums[(n)+j++];
             flag = !flag;
         }
         return res;
