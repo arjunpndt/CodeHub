f3592bf78a24451aaaac21bdb39b166e71a01a9d
Arjun Pandit <arjunpandit0840@gmail.com>
2022-08-06 03:43:10 +0530

    Time: 1 ms (66.03%), Space: 45.7 MB (45.48%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -3,8 +3,10 @@ class Solution {
         boolean flag= true;
         int[] res = new int[nums.length];
         for(int i=0,j=0;i<nums.length;i++){
-            if(flag) res[i]=nums[j];
-            if(!flag) res[i]=nums[(n)+j++];
+            if(flag) 
+                res[i]=nums[j];
+            if(!flag) 
+                res[i]=nums[(n)+j++];
             flag = !flag;
         }
         return res;
