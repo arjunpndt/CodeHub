906af32fa29d509df640d383497a049bc2ea4868
Arjun Pandit <arjunpandit0840@gmail.com>
2023-10-21 09:44:45 +0530

    Time: 47 ms (89.54%), Space: 42.7 MB (68.45%) - LeetHub

--- 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.ts
+++ 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.ts
@@ -2,5 +2,9 @@
 // 	return arr.map((val,index)=>fn(val,index))
 // };
 function map(arr: number[], fn: (n: number, i: number) => number): number[] {
-	return arr.map((val,index)=>fn(val,index))
+    var res=[];
+	for(let i =0;i<arr.length;i++){
+        res[i] =fn(arr[i],i)
+    }
+    return res;
 };
